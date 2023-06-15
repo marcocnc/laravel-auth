0e6024017cdddddbcb83f11df06e1ae05188c466
marcocnc <marcocancelliere56@gmail.com>
2023-06-15 14:14:11 +0200

    separated development environments

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.guest')
+
+@section('content')
+    <h1>Pagina Guest pubblica</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
