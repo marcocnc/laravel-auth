36f66b01ebe2975cd7a8a1f654b495b74556bb2e
marcocnc <marcocancelliere56@gmail.com>
2023-06-15 14:21:13 +0200

    created controller for admin and modified route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,14 +18,11 @@ use App\Http\Controllers\Guest\PageController;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::middleware(['auth', 'verified'])
+                ->name('admin.')
+                ->prefix('admin')
+                ->group(function(){
+                    Route::get('/', [DashboardController::class, 'index'])->name('home');
+                });
 
 require __DIR__.'/auth.php';
