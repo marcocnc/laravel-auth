c1014b2ff439029724d23344ecb88eba86b3de30
marcocnc <marcocancelliere56@gmail.com>
2023-06-18 12:38:09 +0200

    created seeder and saved it in db with fake datas

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($str){
+
+        $slug = Str::slug($str, '-');
+        $original_slug = $slug;
+        $slug_exixts = Post::where('slug', $slug)->first();
+        $c = 1;
+        while($slug_exixts){
+            $slug = $original_slug . '-' . $c;
+            $slug_exixts = Post::where('slug', $slug)->first();
+            $c++;
+        }
+
+        return $slug;
+    }
 }
---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10 ; $i++) {
+            $new_post = new Post();
+            $new_post->name = $faker->sentence();
+            $new_post->slug = Post::generateSlug($new_post->name);
+            $new_post->description = $faker->text(300);
+            $new_post->technologies = $faker->text(300);
+            $new_post->start = $faker->date('Y-m-d');
+            $new_post->end = $faker->date('Y-m-d');
+            $new_post->save();
+        }
+    }
+}
