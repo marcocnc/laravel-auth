775aece058e0c67b6bae98d7c974643fd546a761
marcocnc <marcocancelliere56@gmail.com>
2023-06-18 15:14:47 +0200

    added error messages in create file

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -42,7 +43,7 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
         $form_data = $request->all();
         $form_data['slug'] = Post::generateSlug($form_data['name']);
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -9,7 +9,7 @@
             <div class="alert alert-danger" role="alert">
 
                 <ul>
-                    @foreach ($erros as $error)
+                    @foreach ($errors->all() as $error)
                         <li>{{$error}}</li>
                     @endforeach
                 </ul>
@@ -25,39 +25,63 @@
             <div class="mb-3">
                 <label class="form-label">Nome</label>
                 <input type="text"
-                class="form-control w-75"
+                class="form-control w-75 @error('name')
+                    is-invalid
+                @enderror"
                 id="name"
                 name="name"
                 placeholder="Inserisci nome">
+
+                @error('name')
+                    <p class="text-danger">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Tecnologie usate</label>
                 <textarea
                 type="text"
-                class="form-control w-75"
+                class="form-control w-75 @error('technologies')
+                    is-invalid
+                @enderror"
                 id="technologies"
                 name="technologies"
                 placeholder="Inserisci le tecnologie usate"> </textarea>
+
+                @error('technologies')
+                    <p class="text-danger">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Descrizione</label>
                 <textarea
                 type="text"
-                class="form-control w-75"
+                class="form-control w-75 @error('description')
+                    is-invalid
+                @enderror"
                 id="description"
                 name="description"
                 placeholder="Inserisci le tecnologie usate"> </textarea>
+
+                @error('description')
+                    <p class="text-danger">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Data inizio</label>
                 <input
                 type="date"
-                class="form-control w-25"
+                class="form-control w-25 @error('start')
+                    is-invalid
+                @enderror"
                 id="start"
                 name="start">
+
+                @error('start')
+                    <p class="text-danger">{{$message}}</p>
+                @enderror
             </div>
 
             <div class="mb-3">
