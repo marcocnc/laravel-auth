012cc3f0f368e50ae9c6b4afc0068ec6b9595ed1
marcocnc <marcocancelliere56@gmail.com>
2023-06-18 13:40:18 +0200

    inserted form in create file

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,7 +2,29 @@
 
 @section('content')
     <div class="container pt-5">
-        <h1 class="pb-3 text-center">Create</h1>
+        <h1 class="text-center">Create</h1>
 
+        <form action="{{ route('admin.posts.store') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nome</label>
+                <input type="text" class="form-control w-75" placeholder="Inserisci nome">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Tecnologie usate</label>
+                <textarea type="text" class="form-control w-75" placeholder="Inserisci le tecnologie usate"> </textarea>
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Data inizio</label>
+                <input type="date" class="form-control w-25">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Data fine</label>
+                <input type="date" class="form-control w-25">
+            </div>
+        </form>
     </div>
 @endsection
