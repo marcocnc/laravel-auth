23f49d27807f28fc9bbc3441572cc4943f9a99f6
marcocnc <marcocancelliere56@gmail.com>
2023-06-19 16:00:58 +0200

    added buttons for edit, delete and back in show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,22 +2,30 @@
 
 @section('content')
 <div class="container pt-5">
-    <h1>Nome</h1>
-    <p class="fs-4">{{$post->name}}</p>
+    <div class="title-buttons">
+        <h1 class="text-primary">Nome</h1>
+        <p class="fs-4">{{$post->name}}
+            <a class="btn btn-warning" href="{{route('admin.posts.edit', $post)}}"><i class="fa-solid fa-pencil"></i></a>
+            @include('admin.partials.form-delete')
+        </p>
 
-    <h3>Immagine</h3>
+    </div>
+
+    <h3 class="text-primary">Immagine</h3>
     <img src="{{ asset('storage/' . $post->image_path) }}" alt="" width="500">
 
-    <h3>Descrizione</h3>
+    <h3 class="text-primary">Descrizione</h3>
     <p class="w-75">{{$post->description}}</p>
 
-    <h3>Tecnologie usate</h3>
+    <h3 class="text-primary">Tecnologie usate</h3>
     <p class="w-75">{{$post->technologies}}</p>
 
-    <h3>Data inizio</h3>
+    <h3 class="text-primary">Data inizio</h3>
     <p class="w-75">{{$date_start_formatted}}</p>
 
-    <h3>Data conclusione</h3>
+    <h3 class="text-primary">Data conclusione</h3>
     <p class="w-75">{{$date_end_formatted}}</p>
+
+    <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Torna al Portfolio <i class="fa-solid fa-right-to-bracket ps-1"></i></a>
 </div>
 @endsection
